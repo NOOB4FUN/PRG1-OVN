4384aad8f0eae7274f9a2497cdb2475d8046ad89
NOOB4FUN <90191235+NOOB4FUN@users.noreply.github.com>
2021-09-17 13:40:50 +0200

    lektion fredag 17/9

---
+++ .py
@@ -0,0 +1,19 @@
+a=12
+b=12.0
+c='12.0'
+
+print(f"c+b betyder {float(c) + float(b)}")
+print(f"a+b betyder {float(a) + float(b)}")
+
+print(f"c*b betyder {float(c) + float(b)}")
+print(f"a*b betyder {float(a) + float(b)}")
+
+summa = a*b
+print (summa)
+
+print (summa)
+summan = a+b
+print (summan)
+
+a*b
+
